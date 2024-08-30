e7aee12f7393673ed66e86bb2ea26e9108d78e1a
hichamelg110 <elisejuneaileen@gmail.com>
2024-08-30 12:49:41 -0700

    task

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+import sys
+
+def is_safe(board, row, col, n):
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+def solve_nqueens(n):
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    solutions = []
+
+    def solve(col):
+        if col >= n:
+            solution = []
+            for i in range(n):
+                for j in range(n):
+                    if board[i][j] == 1:
+                        solution.append([i, j])
+            solutions.append(solution)
+            return True
+
+        for i in range(n):
+            if is_safe(board, i, col, n):
+                board[i][col] = 1
+                solve(col + 1)
+                board[i][col] = 0
+
+    solve(0)
+    return solutions
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solutions = solve_nqueens(n)
+    for solution in solutions:
+        print(solution)
+
+if __name__ == "__main__":
+    main()
---
+++ 0x05-nqueens/README.md
@@ -0,0 +1,1 @@
+readme file for project 0x05. N Queens
